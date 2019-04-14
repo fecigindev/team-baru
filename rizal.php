e5537123514328998f0a7d05acbbc536a5731b69
fecigindev <33804459+fecigindev@users.noreply.github.com>
2019-04-14 22:54:15 +0700

    Update rizal.php

--- rizal.php
+++ rizal.php
@@ -1,2 +1,17 @@
 <?php
 echo "hello world";
+// PHP Connect to MySQL
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
